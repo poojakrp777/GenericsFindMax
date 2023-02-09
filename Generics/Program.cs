58320f9bfffdccfbef6eda06c9f4bc2e15f39047
poojakrp777 <poojakrp777@gmail.com>
2023-02-09 13:39:33 +0530

    [POOJA] Using Generics Find Max integer value

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -11,6 +11,9 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find Maximum Using Generics");
+            FindMax findMaximum = new FindMax();
+            Console.WriteLine("Maximum Number is: " + findMaximum.findMaxIntValue(45, 89, 67));
+            Console.ReadLine();
         }
     }
 }
