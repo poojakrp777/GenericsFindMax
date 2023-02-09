42736a4120f8c044f44602d3836c9d83e3b563d4
poojakrp777 <poojakrp777@gmail.com>
2023-02-09 13:35:44 +0530

    [POOJA] Welcome for Using Generics Find max

---
+++ Generics/FindMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class FindMax
+    {
+        public int findMaxIntValue(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
+        }
+    }
+}
